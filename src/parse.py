e5a88a1649ad85d02a6bb293e80085e0c71510cd
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-05-19 18:09:49 -0400

    feat: Update the src/parse.py file with some basic content.

--- src/parse.py
+++ src/parse.py
@@ -1,10 +1,11 @@
 """Parse the bibtex file."""
 
-import bibtexparser
-
-from rich.console import Console
-
 import sys
+from pathlib import Path
+
+import bibtexparser
+import yaml
+from rich.console import Console
 
 if __name__ == "__main__":
     # Main function for the parse script.
@@ -12,12 +13,27 @@ if __name__ == "__main__":
     for i, argument in enumerate(sys.argv):
         print(f"Argument {i:>6}: {argument}")
 
-    bib_database = ""
-    with open(sys.argv[1], encoding="utf-8") as bibtex_file:
-        bib_database = bibtexparser.load(bibtex_file)
+    bibliography = None
+    bib_database_file_name = sys.argv[1]
+    with open(bib_database_file_name, encoding="utf-8") as bibtex_file:
+        bibliography = bibtexparser.load(bibtex_file)
 
     console = Console()
     console.print(
-        f":rocket: Hi! I'm a researcher, Teacher, and Software Developer!"
+        f":rocket: Always run from root to parsing the {bib_database_file_name}"
     )
-    console.print(bib_database.entries)
+    # console.print(bibliography.entries)
+
+    for publication in bibliography.entries:
+        console.print(publication)
+        publication_id = publication["ID"]
+        publication_year = publication["year"]
+        console.print(publication_id)
+        papers_directory = Path(f"papers/{publication_year}-{publication_id}/")
+        papers_directory.mkdir(parents=True, exist_ok=True)
+        publication_file_name = Path(papers_directory / "index.qmd")
+        publication_file_name.touch()
+        publication_file_name.write_text(
+            yaml.dump(publication, allow_unicode=True, default_flow_style=False),
+            encoding="utf-8",
+        )
