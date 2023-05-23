b0721181131215eeb2db9d59f5d2521e05db1649
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-05-23 14:22:03 -0400

    feat: Add the first version of the parse_conference_paper in parse-bibliography.py.

--- scripts/parse-bibliography.py
+++ scripts/parse-bibliography.py
@@ -1,11 +1,37 @@
 """Parse the bibtex file."""
 
 import sys
+import shutil
 from pathlib import Path
 
 import bibtexparser
 import yaml
 from rich.console import Console
+
+from typing import Dict
+
+def parse_conference_paper(current_publication: Dict[str, str]):
+    """Parse a conference paper, noted because it uses a booktitle."""
+    if current_publication.get("booktitle"):
+        publication_id = publication["ID"]
+        publication_year = publication["year"]
+        publication_abstract = publication["abstract"]
+        publication_booktitle = publication["booktitle"]
+        publication["description"] = f"<em>{publication_booktitle}</em>"
+        publication_abstract = publication_abstract.replace("\n", " ")
+        publication["abstract"] = publication_abstract
+        publication_year = publication["year"]
+        publication["date"] = publication_year
+        console.print(publication_id)
+        papers_directory = Path(f"papers/{publication_year}-{publication_id}/")
+        papers_directory.mkdir(parents=True, exist_ok=True)
+        publication_file = Path(papers_directory / "index.qmd")
+        publication_file.touch()
+        publication_file.write_text(
+            f"---\n{yaml.dump(publication, allow_unicode=True, default_flow_style=False)}\n---",
+            encoding="utf-8",
+        )
+
 
 if __name__ == "__main__":
     print(f"Arguments count: {len(sys.argv)}")
@@ -27,22 +53,9 @@ if __name__ == "__main__":
     )
     console.print(len(bibliography.entries))
 
+    papers_directory = Path("papers/")
+    shutil.rmtree(papers_directory)
+
     for publication in bibliography.entries:
-        console.print(publication)
-        console.print(".")
-        publication_id = publication["ID"]
-        publication_year = publication["year"]
-        publication_abstract = publication["abstract"]
-        publication_booktitle = publication["booktitle"]
-        publication["description"] = f"<em>{publication_booktitle}</em>"
-        publication_abstract = publication_abstract.replace("\n", " ")
-        publication["abstract"] = publication_abstract
-        console.print(publication_id)
-        papers_directory = Path(f"papers/{publication_year}-{publication_id}/")
-        papers_directory.mkdir(parents=True, exist_ok=True)
-        publication_file = Path(papers_directory / "index.qmd")
-        publication_file.touch()
-        publication_file.write_text(
-            f"---\n{yaml.dump(publication, allow_unicode=True, default_flow_style=False)}\n---",
-            encoding="utf-8",
-        )
+        console.print(".", end=" ")
+        parse_conference_paper(publication)
