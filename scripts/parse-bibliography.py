c8c4c6c028b1c2e531d4630bbc31ff7df5fe9a40
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-06-01 08:36:00 -0400

    coms: Remove commented-out content in the parse-bibliography.py script.

--- scripts/parse-bibliography.py
+++ scripts/parse-bibliography.py
@@ -176,9 +176,6 @@ def create_categories(publication: Dict[str, str]) -> None:
         # do not allow more than four entries for keywords
         delete_elements_beyond_max_size(found_keyword_list, MAX_KEYWORD_SIZE)
         publication["categories"] = f"[{', '.join(found_keyword_list)}]"
-        # console.print(publication_title)
-        # console.print(found_keyword_list)
-        # console.print()
 
 
 def create_publication_footer(publication: Dict[str, str]) -> str:
