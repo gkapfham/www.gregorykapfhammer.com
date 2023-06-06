e494864c5dfc5a92058774210c77a7f419265246
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-06-06 19:47:26 -0400

    feat: Add extra options to the minify-files.py script.

--- scripts/minify-files.py
+++ scripts/minify-files.py
@@ -11,60 +11,69 @@ from rich.console import Console
 
 console = Console()
 
+DEFAULT_CONSOLE_STYLE = "bold blue"
 
-def minify_files(directory):
+
+def minify_files(source_directory: str, destination_directory: str) -> None:
     """Minify all of the files in the project directory."""
     # recursively iterate through all files and directories
-    for filename in os.listdir(directory):
-        # extract the current filename
-        filepath = os.path.join(directory, filename)
+    for current_file_name in os.listdir(source_directory):
+        # create the current filename subject to analysis
+        analysis_file_path = os.path.join(source_directory, current_file_name)
+        # create the current filename for writing to file system
+        saving_file_path = os.path.join(destination_directory, current_file_name)
         # dealing with a file and thus it is a candidate
         # for the minification process
-        if os.path.isfile(filepath):
+        if os.path.isfile(analysis_file_path):
             # extract the extension from this filename so
             # that the script can check if it is one of
             # the types of files that can be minified
-            extension = os.path.splitext(filename)[1].lower()
+            extension = os.path.splitext(current_file_name)[1].lower()
             # minify the CSS file
             if extension == ".css":
                 try:
-                    minified_content = csscompressor.compress(open(filepath).read())
-                    with open(filepath, "w") as file:
+                    minified_content = csscompressor.compress(
+                        open(analysis_file_path).read()
+                    )
+                    with open(saving_file_path, "w") as file:
                         file.write(minified_content)
-                    console.print(f"CSS {filepath} minified")
+                    console.print(f"CSS: Minifying {analysis_file_path}")
                 except ValueError:
-                    console.print(f"Could not minifiy file {filepath}")
+                    console.print(f"CSS: Could not minifiy file {analysis_file_path}")
             # minify the HTML file
             elif extension == ".html":
                 minified_content = htmlmin.minify(
-                    open(filepath).read(),
+                    open(analysis_file_path).read(),
                     remove_comments=True,
                     remove_empty_space=True,
                 )
-                with open(filepath, "w") as file:
+                with open(saving_file_path, "w") as file:
                     file.write(minified_content)
-                console.print(f"HTML {filepath} minified")
+                console.print(f"HTML: Minifying {analysis_file_path}")
             # minify the JS file
             elif extension == ".js":
-                minified_content = str(rjsmin.jsmin(open(filepath).read()))
-                with open(filepath, "w") as file:
+                minified_content = str(rjsmin.jsmin(open(analysis_file_path).read()))
+                with open(saving_file_path, "w") as file:
                     file.write(minified_content)
-                console.print(f"JS {filepath} minified")
+                console.print(f"JS: Minifying {analysis_file_path}")
             # do not have a minifier for a specific
             # file and thus this file should be skipped
             else:
-                console.print(f"{filepath} skipped")
+                console.print(f"Skipping {analysis_file_path}")
         # found a directory, so recursively traverse into
-        # so that minification process can continue until done
-        elif os.path.isdir(filepath):
-            minify_files(filepath)
+        # so that minification process can continue until done;
+        # keep using the same destination_directory as that will
+        # not change during the recursive traversal process
+        elif os.path.isdir(analysis_file_path):
+            minify_files(analysis_file_path, destination_directory)
 
 
 def main() -> None:
     """Perform the steps for the main function."""
     # parse the command-line arguments using argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--directory")
+    parser.add_argument("-s", "--source")
+    parser.add_argument("-d", "--destination")
     parser.add_argument("-f", "--force", action="store_true")
     parser.add_argument("-v", "--verbose", action="store_true")
     # create the argument parser
@@ -73,18 +82,35 @@ def main() -> None:
     # all of the files (i.e., do not run during preview)
     if not os.getenv("QUARTO_PROJECT_RENDER_ALL") and not args.force:
         sys.exit()
-    # define the default directory that will contain
+    # define the default source directory that will contain
     # the contents that are going to be minified
-    directory_path = "_site"
-    # there is a directory, so use it instead of
-    # the default directory
-    if args.directory is not None:
-        directory_path = args.directory
+    source_directory_path = "_site"
+    # define the default destination directory that will
+    # contain the minified contents
+    destination_directory_path = "_site"
+    # there is a directory for the source, so use it instead of
+    # the default directory for the source
+    if args.source is not None:
+        source_directory_path = args.source
+    # there is a directory for the destination, so use it instead of
+    # the default directory for the destination
+    if args.destination is not None:
+        destination_directory_path = args.destination
+    # provide the debugging information
+    if args.verbose is True:
+        console.print(
+            f":clap: Using the source directory of {destination_directory_path}\n",
+            style=DEFAULT_CONSOLE_STYLE,
+        )
+        console.print(
+            f":clap: Using the source directory of {source_directory_path}\n",
+            style=DEFAULT_CONSOLE_STYLE,
+        )
     # perform the minification of all of the files
     # inside of the directory; note that this is
     # a destructive operation that changes the
     # contents of the specified directory
-    minify_files(directory_path)
+    minify_files(source_directory_path, destination_directory_path)
 
 
 if __name__ == "__main__":
