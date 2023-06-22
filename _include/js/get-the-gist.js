13b45551e47a90bdf792f9e481d64c7c9494da28
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-06-22 16:27:43 -0400

    fix: Decrease the strokeWidth and add padding: 2 for const config in get-the-gist.js for rough-notation.

--- _include/js/get-the-gist.js
+++ _include/js/get-the-gist.js
@@ -2,7 +2,7 @@
   // import the rough notation module
   import { annotate, annotationGroup } from 'https://unpkg.com/rough-notation?module';
   // define a highlight that will be a multiple-line underline that appears for all annotated text
-  const config = { type: 'underline', multiline: true, iterations: 1, strokeWidth: 4, animationDuration: 200, color: '#EF6C00' };
+  const config = { type: 'underline', multiline: true, iterations: 1, strokeWidth: 3, animationDuration: 200, color: '#EF6C00', padding: 2 };
   // extract all of the elements that are designated as special
   // and thus subject to receiving the rough notation underline
   const nl = document.querySelectorAll('.gist');
