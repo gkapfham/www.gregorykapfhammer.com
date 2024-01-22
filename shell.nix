733839365fb5fef48d58c8dc360ed78ef311f9a1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-01-22 15:42:14 -0500

    chore: Add starting version of the shell.nix file.

---
+++ shell.nix
@@ -0,0 +1,33 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/04220ed6763637e5899980f98d5c8424b1079353.tar.gz";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+  pkgs.mkShell {
+    packages = with pkgs; [
+      (python311.withPackages (ps: with ps; [
+        bibtexparser
+        csscompressor
+        htmlmin
+        # minify-html
+        pyyaml
+        rich
+        rjsmin
+        jupyterlab
+        jupyterlab-git
+        pip
+        pydocstyle
+        quarto
+      ]))
+      git
+      pipx
+      poetry
+      prettierd
+      python3
+      quarto
+      zsh
+    ];
+  shellHook = ''
+    echo "✨ Running Quarto Preview with the 'develop' profile"
+    quarto preview --profile develop
+  '';
+  }
