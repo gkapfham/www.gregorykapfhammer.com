d63e8979ff9537804b75811a4a4070c1c39ff0b7
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-07-24 12:33:28 -0400

    refactor: Try a completely new approach to package management in shell.nix.

--- shell.nix
+++ shell.nix
@@ -1,29 +1,108 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/04220ed6763637e5899980f98d5c8424b1079353.tar.gz";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
+ # pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/d16116afd7ab80a3eb3311b3bc96fa7bba383638.tar.gz") {};
+ pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/04220ed6763637e5899980f98d5c8424b1079353.tar.gz") {};
+ rpkgs = builtins.attrValues {
+  inherit (pkgs.rPackages) quarto ggplot2 knitr reticulate rmarkdown withr;
+ };
+ pypkgs = builtins.attrValues {
+   inherit (pkgs.python311Packages) rich ;
+ };
+ tex = (pkgs.texlive.combine {
+   inherit (pkgs.texlive) scheme-small amsmath framed fvextra environ fontawesome5 orcidlink pdfcol tcolorbox tikzfill;
+  });
+ system_packages = builtins.attrValues {
+  inherit (pkgs) R glibcLocalesUtf8 quarto python311;
+};
+  in
   pkgs.mkShell {
-    packages = with pkgs; [
-      (python311.withPackages (ps: with ps; [
-        bibtexparser
-        csscompressor
-        htmlmin
-        pyyaml
-        rich
-        rjsmin
-        jupyterlab
-        jupyterlab-git
-        pip
-        pydocstyle
-        quarto
-      ]))
-    ];
-  shellHook = ''
-    echo "󰁡 Setting JUPYTER_PATH and PYTHONPATH for quarto"
-    export JUPYTER_PATH=${pkgs.python311Packages.jupyterlab}/share/jupyter
-    export PYTHONPATH=$PYTHONPATH:${pkgs.python311Packages.ipykernel}/${pkgs.python311.sitePackages}
-    echo " Suggested Commands: 'quarto preview' or 'quarto render'"
-    echo "󰢌 Poetry Reminder: Use 'poetry shell' before running Python scripts"
-    echo " Example Script: 'python scripts/minify-files.py --verbose --force'"
-  '';
+    LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
+    LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+
+    buildInputs = [ pypkgs rpkgs tex system_packages  ];
+      
   }
+
+# let
+#   nixpkgs = import <nixpkgs> {};
+#   myPython = nixpkgs.python3.withPackages (ps: with ps; [
+#     jupyter
+#     ipython
+#     rich
+#     # Add your Python packages here
+#   ]);
+# in
+#   nixpkgs.mkShell {
+#     buildInputs = [
+#       (nixpkgs.quarto.override { python3 = myPython; })
+#       # Add other packages here
+#     ];
+#   }
+
+# let
+#   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/04220ed6763637e5899980f98d5c8424b1079353.tar.gz";
+#   pkgs = import nixpkgs { config = {}; overlays = []; };
+#   myPython = pkgs.python311.withPackages (ps: with ps; [
+#     bibtexparser
+#     csscompressor
+#     htmlmin
+#     pyyaml
+#     rich
+#     rjsmin
+#     ipykernel
+#     jupyterlab
+#     jupyterlab-git
+#     pip
+#     pydocstyle
+#   ]);
+# in
+#   pkgs.mkShell {
+#     packages = with pkgs; [
+#       myPython
+#       (pkgs.quarto.override { python3 = myPython; })
+#     ];
+#   shellHook = ''
+#     # echo "󰁡 Setting JUPYTER_PATH and PYTHONPATH for quarto"
+#     # export JUPYTER_PATH=${pkgs.python311Packages.jupyterlab}/share/jupyter
+#     # export PYTHONPATH=$PYTHONPATH:${pkgs.python311Packages.ipykernel}/${pkgs.python311.sitePackages}
+#     echo " Suggested Commands: 'quarto preview' or 'quarto render'"
+#     echo "󰢌 Poetry Reminder: Use 'poetry shell' before running Python scripts"
+#     echo " Example Script: 'python scripts/minify-files.py --verbose --force'"
+#   '';
+#   }
+
+# let
+#   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/04220ed6763637e5899980f98d5c8424b1079353.tar.gz";
+#   pkgs = import nixpkgs { config = {}; overlays = []; };
+# in
+#   pkgs.mkShell {
+#     packages = with pkgs; [
+#       myPython = (python311.withPackages (ps: with ps; [
+#         bibtexparser
+#         csscompressor
+#         htmlmin
+#         pyyaml
+#         rich
+#         rjsmin
+#         ipykernel
+#         jupyterlab
+#         jupyterlab-git
+#         pip
+#         pydocstyle
+#         quarto
+#       ]))
+#       quarto.override { python3 = myPython; }
+#     ];
+#   shellHook = ''
+#     # echo "󰁡 Setting JUPYTER_PATH and PYTHONPATH for quarto"
+#     # export JUPYTER_PATH=${pkgs.python311Packages.jupyterlab}/share/jupyter
+#     # export PYTHONPATH=$PYTHONPATH:${pkgs.python311Packages.ipykernel}/${pkgs.python311.sitePackages}
+#     echo " Suggested Commands: 'quarto preview' or 'quarto render'"
+#     echo "󰢌 Poetry Reminder: Use 'poetry shell' before running Python scripts"
+#     echo " Example Script: 'python scripts/minify-files.py --verbose --force'"
+#   '';
+#   }
